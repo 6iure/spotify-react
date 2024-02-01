311c6186285c4881f2ef1f27006bfe51fbaae171
Iure Finotti <iure@sysout.com.br>
2024-02-01 13:45:35 -0300

    correção no main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Sidebar from './sidebar/sidebar'
 import Header from './header/header';
-import Main from './main/main';
 import Footer from './footer/footer';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -15,7 +14,6 @@ function App() {
     <div>
     <Sidebar/>
     <Header/>
-    <Main/>
     <Footer/>
     </div>
   );
