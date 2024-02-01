7b5bda86883ce54d0965076f1bc69bdefab330e5
Iure Santiago <iuresantiag016@gmail.com>
2024-01-31 23:17:02 -0300

    ajuste no header e há de se arrumar o main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Sidebar from './sidebar/sidebar'
 import Header from './header/header';
+import Main from './main/main';
 import Footer from './footer/footer';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -14,6 +15,7 @@ function App() {
     <div>
     <Sidebar/>
     <Header/>
+    <Main/>
     <Footer/>
     </div>
   );
